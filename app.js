aa7eba22a38983677afcc69658dd11d7005f5c75
SerenaElric <agraham43@student.alamo.edu>
2026-01-30 14:07:56 -0600

    variables

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+var dragon=250; // global
+let color= 'green'; // string
+let dragonName='Shinron';
+dragon = dragon-25; 
+
+{// block
+    let player= 'Bob';
+    let health=100;
+}
+
+const weapon= {
+    type: 'sword',
+    attack: 25,
+
+}
+
+console.log(dragon);
+console.log(color);
+console.log(dragonName);
+console.log(weapon);
