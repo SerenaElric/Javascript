bf9ab5adefdc620af92b9d0e988352c540328ac7
SerenaElric <agraham43@student.alamo.edu>
2026-02-05 17:01:30 -0600

    functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var dragon=250; // global
+let dragon=250; // global
 let color= 'green'; // string
 let dragonName='Shinron';
 dragon = dragon-25; 
@@ -18,3 +18,34 @@ console.log(dragon);
 console.log(color);
 console.log(dragonName);
 console.log(weapon);
+
+// function homework
+
+function greeting (username, age){
+    console.log(`Hello my name is ${username}.`);
+    console.log(`I am ${age} years old.`)
+}
+
+greeting ("Amanda", 33);
+greeting ("Stacey", 40);
+
+const person1 = {
+    firstName: "Amanda",
+    lastName: "Graham",
+    sayHello: () => console.log ("Hi, I'm Amanda!"),
+
+}
+
+const person2 = {
+    firstName: "Stacey",
+    lastName: "Barrera",
+    sayHello: () => console.log ("Hello, my name is Stacey.")
+}
+
+console.log(person1.firstName);
+console.log(person1.lastName);
+person1.sayHello();
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+person2.sayHello();
